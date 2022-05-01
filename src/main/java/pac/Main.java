c086d80ebe059a9d332ca7bf66bf2baeaaf8de64
Carlos Morales <carlosm.moralesp@outlook.com>
2022-05-01 15:01:10 +0200

    add changes

--- src/main/java/pac/Main.java
+++ src/main/java/pac/Main.java
@@ -8,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Inicio");
+
         Configuration cfg = new Configuration().configure();
         SessionFactory sf = cfg.buildSessionFactory(new StandardServiceRegistryBuilder().configure().build());
         Session s = sf.openSession();
